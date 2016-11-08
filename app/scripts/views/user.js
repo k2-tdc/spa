5f39117d0d62b723ea4ccd0030f41097a3452ea2
Tommy Ho <tommy8694@gmail.com>
2016-11-08 12:52:02 +0800

    logout

--- app/scripts/views/user.js
+++ app/scripts/views/user.js
@@ -11,6 +11,18 @@ Hktdc.Views = Hktdc.Views || {};
 
     el: '#user-profile-menu',
 
+    events: {
+      'click #logout': 'doLogout'
+    },
+
+    doLogout: function() {
+      // console.log(Cookies.get());
+      Cookies.remove('ACCESS-TOKEN');
+      Cookies.remove('REFRESH-TOKEN');
+      var oauthUrl = window.Hktdc.Config.OAuthLoginUrl + '?redirect_uri=' + encodeURI(window.Hktdc.Config.SPAHomeUrl);
+      window.location.href = oauthUrl;
+    },
+
     initialize: function() {
       this.render();
     },
