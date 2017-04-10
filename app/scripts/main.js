f3e146e283d5d80fadc1e45dbc227f8c8e5cba23
Tommy Ho <tommy8694@gmail.com>
2017-04-10 17:25:50 +0800

    change oauth url

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -67,9 +67,9 @@ window.Hktdc = {
         SPAHomePath: '/vicosysspa/',
         SPADomain: 'https://workflowuat.tdc.org.hk',
         logoutURL: 'https://corpsso.tdc.org.hk/adfs/ls/?wa=wsignout1.0',
-        OAuthLoginPath: '/workflow/oauth2/login',
-        OAuthGetTokenPath: '/workflow/oauth2/token',
-        OAuthGetUserIDPath: '/workflow/oauth2/tokeninfo'
+        OAuthLoginPath: '/oauth/oauth2/login',
+        OAuthGetTokenPath: '/oauth/oauth2/token',
+        OAuthGetUserIDPath: '/oauth/oauth2/tokeninfo'
       },
 
       // REAL UAT VM - uat test
