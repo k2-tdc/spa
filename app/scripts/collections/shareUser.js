f706286b2ea8e15800707c4cdef37fb41c23762d
Tommy Ho <tommy8694@gmail.com>
2017-05-17 19:40:32 +0800

    share user list api changed

--- app/scripts/collections/shareUser.js
+++ app/scripts/collections/shareUser.js
@@ -8,7 +8,7 @@ Hktdc.Collections = Hktdc.Collections || {};
   Hktdc.Collections.ShareUser = Backbone.Collection.extend({
     url: function(type) {
       var typePath = type ? '&type=' + type : '';
-      return Hktdc.Config.apiURL + '/users/' + Hktdc.Config.userID + '/share-user?process=CHSW' + typePath;
+      return Hktdc.Config.apiURL + '/users/' + Hktdc.Config.userID + '/shared-work-list-owners?process=CHSW' + typePath;
     },
 
     model: Hktdc.Models.Employee
